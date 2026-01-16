1db0bc4c90d5de40a1fd01df4ead214b22951979
Samarth Funde <122684505+samarthfunde@users.noreply.github.com>
2026-01-17 00:13:40 +0530

    Delete terraform_day_1 directory

--- terraform_day_1/install_nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update -y
-sudo apt-get install nginx -y
-sudo systemctl start nginx
-sudo systemctl enable nginx
-
-echo "<h1> I Created Infrastructure using Terraform</h1>" | sudo tee /var/www/html/index.html
